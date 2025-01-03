b2930746ebccec25aeec9c41d9e48bc89e505b6d
hkhangus <huukhangtc@gmail.com>
2025-01-03 22:21:57 +0700

    chore: update search bar

--- src/components/SeachBar.tsx
+++ src/components/SeachBar.tsx
@@ -1,19 +1,23 @@
+import React from "react";
 
 type SearchBarProps = {
   placeholder?: string;
   value: string,
   onSubmit: (value: string) => void
   onChange: (value: string) => void
+  className?: string
 }
 
-const SearchBar = (props: SearchBarProps) => {
-  const { placeholder = "Search", value, onChange, onSubmit } = props;
+const SearchBar = React.forwardRef<HTMLFormElement, SearchBarProps>((props, ref) => {
+  const { placeholder = "Search", value, onChange, onSubmit, ...rest } = props;
   return (
     <form
       onSubmit={(e) => {
         e.preventDefault();
         onSubmit((e.target as HTMLFormElement).query.value.toString().trim());
       }}
+      ref={ref}
+      {...rest}
     >
       <label className="input input-bordered flex items-center gap-2 pr-0">
         <input
@@ -22,7 +26,7 @@ const SearchBar = (props: SearchBarProps) => {
           className="grow"
           value={value}
           placeholder={placeholder}
-          onChange={(e) => onChange(e.target.value.toString().trim())}
+          onChange={(e) => onChange(e.target.value.toString())}
         />
         <button type="submit" className="btn btn-ghost">
           <i className="icon-search" />
@@ -30,6 +34,6 @@ const SearchBar = (props: SearchBarProps) => {
       </label>
     </form>
   )
-}
+})
 
 export default SearchBar;
