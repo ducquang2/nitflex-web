1eb3658ae496cf24f0b4bc7375bd461503cf2d4d
hkhangus <huukhangtc@gmail.com>
2025-01-03 22:22:24 +0700

    chore: add constants + types

--- src/libs/utils/types.ts
+++ src/libs/utils/types.ts
@@ -20,3 +20,93 @@ export type Movie = {
   vote_average: number
   vote_count: number
 }
+
+export type Genre = {
+  ID: number
+  Name: string
+}
+
+export type Collection = {
+  ID: number
+  Name: string
+  poster_path: string
+  backdrop_path: string
+}
+
+export type ProductionCompany = {
+  ID: number
+  Name: string
+}
+
+export type ProductionCountry = {
+  iso_3166_1: string
+  Name: string
+}
+
+export type SpokenLanguage = {
+  iso_639_1: string
+  Name: string
+}
+
+export type MovieInfo = {
+  Adult: boolean
+  Budget: number
+  Genres: Genre[]
+  Homepage: string
+  ID: number
+  Overview: string
+  Popularity: number
+  Revenue: number
+  Runtime: number
+  Status: string
+  Tagline: string
+  Title: string
+  Video: boolean
+  backdrop_path: string
+  belongs_to_collection: Collection
+  imdb_id: string
+  original_language: string
+  original_title: string
+  poster_path: string
+  production_companies: ProductionCompany[]
+  production_countries: ProductionCountry[]
+  release_date: string
+  spoken_languages: SpokenLanguage[]
+  vote_average: number
+  vote_count: number
+}
+
+export type Cast = {
+  cast_id: number
+  Character: string
+  credit_id: string
+  ID: number
+  Name: string
+  Order: number
+  profile_path: string
+}
+
+export type Crew = {
+  credit_id: string
+  Department: string
+  ID: number
+  Job: string
+  Name: string
+  profile_path: string
+}
+
+export type CastInfo = {
+  id: number
+  name: string
+  overview: string
+  adult: boolean
+  biography: string
+  birthday: string
+  deathday: string
+  gender: number
+  imdb_id: string
+  homepage: string
+  also_known_as: Array<string>
+  place_of_birth: string
+  profile_path: string
+}
