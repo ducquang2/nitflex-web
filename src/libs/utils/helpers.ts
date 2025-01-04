bae0b966aa17fcbcb2bc6479e050176162e9a3f5
ducquang2 <nducquangg@gmail.com>
2025-01-04 11:17:58 +0700

    chore: add new helper func for add image prefix

--- src/libs/utils/helpers.ts
+++ src/libs/utils/helpers.ts
@@ -34,3 +34,20 @@ export function splitToSmallChunks(array: Array<Movie>, n: number) {
   }
   return result
 }
+
+/**
+ * @description Adds image prefix to the image path.
+ *
+ * @param image - The image path.
+ * @returns - The image path with the prefix.
+ */
+export function addImagePrefix(image?: string) {
+  if (!image) {
+    return 'https://dummyimage.com/600x1400/000/fff.png&text=no_image'
+  }
+  if (image.startsWith('http')) {
+    return image
+  }
+
+  return `https://image.tmdb.org/t/p/w500${image}`
+}
