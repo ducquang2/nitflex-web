f14a82312259d0250e78e281d1c3d774ae599601
hkhangus <huukhangtc@gmail.com>
2025-01-12 00:57:32 +0800

    feat: implement search with llm

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 
 import classNames from "classnames";
 
-import { get_movies } from "@apis/movies";
+import { get_llm_movies, get_movies } from "@apis/movies";
 import { addImagePrefix } from "@libs/utils/helpers";
 import { MovieInfo } from "@libs/utils/types";
 
@@ -25,11 +25,11 @@ const NavBar = (props: NavBarProps) => {
 
   const showSearchBar = SHOW_SEARCH_BAR_PATHS.some((pattern) => pattern.test(location.pathname));
 
-
   const [isLoading, setIsLoading] = useState(false);
   const [canShowDropdown, setCanShowDropdown] = useState(false);
   const [movies, setMovies] = useState<Array<MovieInfo>>([]);
   const [query, setQuery] = useState("");
+  const [isLLMSearch, setIsLLMSearch] = useState(false);
 
   const handleLogout = () => {
     setToken(null);
@@ -53,19 +53,24 @@ const NavBar = (props: NavBarProps) => {
     const getMovies = async () => {
       if (!query) return;
 
-      const responeMovies = await get_movies({ query, page: 1 });
+      try {
+        const responseMovies = isLLMSearch ? await get_llm_movies({ query }) : await get_movies({ query, page: 1 });
 
-      setIsLoading(false);
+        setIsLoading(false);
 
-      if (!responeMovies) return;
-      if (responeMovies.results?.length === 0) return;
+        if (!responseMovies) return;
+        if (responseMovies.results?.length === 0) return;
 
-      setMovies(responeMovies.results.slice(0, 10));
+        setMovies(responseMovies.results.slice(0, 10));
+      } catch (error) {
+        console.error("Error fetching movies:", error);
+        setIsLoading(false);
+      }
     };
 
     const debounce = setTimeout(() => getMovies(), 500);
     return () => clearTimeout(debounce);
-  }, [query]);
+  }, [query, isLLMSearch]);
 
   useEffect(() => {
     if (location.pathname.includes('/movies')) {
@@ -86,47 +91,54 @@ const NavBar = (props: NavBarProps) => {
         </Link>
       </div>
       {showSearchBar && (
-        <div className="flex flex-1 justify-center items-center dropdown dropdown-bottom">
-          <SearchBar className="w-4/5" value={query} onChange={handleSearchInputChange} onSubmit={handleSearchSubmit} />
-          {query && canShowDropdown && (
-            <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-4/5 p-2 mt-2 shadow">
-              {isLoading ? (
-                <div className="h-14 flex justify-center items-center">
-                  <span className="loading loading-dots loading-md" />
-                </div>
-              ) :
-                <>
-                  {movies.map((movie) => (
-                    <li key={movie.Id}>
-                      <Link to={`/movies/${movie.TmdbId}`}>
-                        <div className="flex gap-2 items-center">
-                          <img
-                            src={addImagePrefix(movie.PosterPath)}
-                            alt={movie.Title} className="h-[64px] w-fit object-cover rounded-btn"
-                          />
-                          <div className="flex flex-col gap-1">
-                            <p className="font-medium">
-                              {movie.Title}
-                            </p>
+        <div className="flex-1 justify-center items-center">
+          <div className="flex flex-1 justify-center items-center dropdown dropdown-bottom">
+            <SearchBar className="w-4/5" value={query} onChange={handleSearchInputChange} onSubmit={handleSearchSubmit} />
+
+            {query && canShowDropdown && (
+              <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-4/5 p-2 mt-2 shadow">
+                {isLoading ? (
+                  <div className="h-14 flex justify-center items-center">
+                    <span className="loading loading-dots loading-md" />
+                  </div>
+                ) :
+                  <>
+                    {movies.map((movie) => (
+                      <li key={movie.Id}>
+                        <Link to={`/movies/${movie.TmdbId}`}>
+                          <div className="flex gap-2 items-center">
+                            <img
+                              src={addImagePrefix(movie.PosterPath)}
+                              alt={movie.Title} className="h-[64px] w-fit object-cover rounded-btn"
+                            />
+                            <div className="flex flex-col gap-1">
+                              <p className="font-medium">
+                                {movie.Title}
+                              </p>
+                            </div>
                           </div>
-                        </div>
-                      </Link>
+                        </Link>
+                      </li>
+                    ))}
+                    <li>
+                      <button
+                        onClick={() => {
+                          setCanShowDropdown(false);
+                          navigate(`/movies?query=${query}`)
+                        }}
+                        className="btn btn-ghost">
+                        View all
+                      </button>
                     </li>
-                  ))}
-                  <li>
-                    <button
-                      onClick={() => {
-                        setCanShowDropdown(false);
-                        navigate(`/movies?query=${query}`)
-                      }}
-                      className="btn btn-ghost">
-                      View all
-                    </button>
-                  </li>
-                </>
-              }
-            </ul>
-          )}
+                  </>
+                }
+              </ul>
+            )}
+          </div>
+          <label className="label cursor-pointer ml-2">
+            <span className="label-text mr-2 w-12">{isLLMSearch ? "LLM" : "Simple"}</span>
+            <input type="checkbox" className="toggle toggle-primary" checked={isLLMSearch} onChange={() => setIsLLMSearch(!isLLMSearch)} />
+          </label>
         </div>
       )}
       <div className="flex-none">
