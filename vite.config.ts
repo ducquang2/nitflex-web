7abdb779895b1119d6439fe4ac6431602071eb7c
ducquang2 <nducquangg@gmail.com>
2025-01-14 21:10:41 +0700

    feat: deploy on gh page

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default ({ mode = 'development' }: UserConfig) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return defineConfig({
-    base: '/nitflex',
+    base: '/nitflex/',
     server: {
       port: process.env.VITE_PORT ? +process.env.VITE_PORT : 4444,
     },
