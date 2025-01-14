a82121e5a33a6d89e0fcc72da71b9ecee33ed0c2
ducquang2 <nducquangg@gmail.com>
2025-01-14 21:05:28 +0700

    feat: ci/cd for fe

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv, UserConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv, UserConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
-export default ({ mode = "development" }: UserConfig) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+export default ({ mode = 'development' }: UserConfig) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return defineConfig({
-    base: "./",
+    base: '/nitflex',
     server: {
       port: process.env.VITE_PORT ? +process.env.VITE_PORT : 4444,
     },
     plugins: [react(), tsconfigPaths()],
     resolve: {
       alias: {
-        "@": "/src",
+        '@': '/src',
       },
     },
-  });
-};
+  })
+}
